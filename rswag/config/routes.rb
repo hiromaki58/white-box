cfcff45bf2e9c3b7500d929a165a900c3fcce9e4
Hiro Maki <hiromaki58@gmail.com>
2023-09-10 10:43:29 -0400

    Able to access to http://localhost:3000/api-docs/ and see the document

--- rswag/config/routes.rb
+++ rswag/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  # Rswag routes
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  get 'sessions/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  post '/auth/login', to: 'sessions#create'
+  namespace :api do
+    namespace :v1 do
+      resources :todos, only: [:index]
+    end
+  end
 end
