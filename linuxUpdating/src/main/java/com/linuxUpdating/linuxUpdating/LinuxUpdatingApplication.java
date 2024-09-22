31bebf09fc5acbc027d45c7d31499f6ede00ae06
Hiro Maki <hiromaki58@gmail.com>
2024-09-21 22:03:57 -0400

    [info]try to make jar. not success

---
+++ linuxUpdating/src/main/java/com/linuxUpdating/linuxUpdating/LinuxUpdatingApplication.java
@@ -0,0 +1,13 @@
+package com.linuxUpdating.linuxUpdating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxUpdatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxUpdatingApplication.class, args);
+	}
+
+}
--- linux_update/src/main/java/Main.java
+++ linux_update/src/main/java/Main.java
@@ -3,10 +3,15 @@ package main.java;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
-      String csvFile = "src/test/java/mockInput.csv";
+	public static void main(String[] args){
+		if(args.length == 0){
+			System.out.println("No path is set.");
+			return;
+		}
 
-      List<String> ipAddrList = CsvReader.getIpAddrList(csvFile);
-      System.out.println("Get ip addr list");
-    }
+		String csvFile = "src/test/java/mockInput.csv";
+
+		List<String> ipAddrList = CsvReader.getIpAddrList(csvFile);
+		System.out.println("Get ip addr list");
+	}
 }
