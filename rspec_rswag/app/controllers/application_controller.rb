0cf16f2bb99a238651b120e500520a74dcd224ce
Hiro Maki <hiromaki58@gmail.com>
2024-10-06 23:32:12 -0400

    [remove]clean up the old project

--- rspec_rswag/app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::API
-  before_action :authenticated?
-
-  def authenticated?
-    if request.headers[:Authorization] != 'Barer abcdefg123456'
-      render status: 401, json: { error: 'Unauthorized' }
-    end
-  end
-end
