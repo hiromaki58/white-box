ced9540c5f10f3f7fe9bfc98325889e16cd46837
Hiro Maki <hiromaki58@gmail.com>
2024-10-14 23:59:12 -0400

    [change]change csv readers to match up the open-close principle

--- linux_updating/src/main/java/springframework/linuxUpdating/service/CsvReader.java
+++ linux_updating/src/main/java/springframework/linuxUpdating/service/CsvReader.java
@@ -12,32 +12,44 @@ public class CsvReader {
     /**
      * 
      * @param filePath
+     * @param columnIndex
+     * @param columnTitle
      * @return
      */
-    public List<String> getIpAddrList(String filePath) {
+    private List<String> getColumnInfoList(String filePath, int columnIndex, String columnTitle){
         final int csvColumnCount = 4;
-        final int ipAddrPositionNum = 2;
-        List<String> ipAddrList = new ArrayList<>();
+        List<String> inputList = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
             String line;
-            while ((line = br.readLine()) != null) {
-                String[] column = line.split(",");
+            while((line = br.readLine()) != null){
+                String[] columnList = line.split(",");
 
-                if (column.length > csvColumnCount - 1 && column[2].trim().isEmpty()) {
-                    System.out.println("Warning: Missing the 3rd value (GIP)");
+                if(columnList.length > csvColumnCount - 1 && columnList[columnIndex].trim().isEmpty()){
+                    System.err.println("Warning : Missing the value in column " + columnTitle);
                 }
 
-                if (column.length > csvColumnCount - 1) {
-                    ipAddrList.add(column[ipAddrPositionNum]);
+                if(columnList.length > csvColumnCount - 1){
+                    inputList.add(columnList[columnIndex]);
                 }
             }
         }
-        catch(IOException e) {
-            System.out.println("getIpAddrList error");
+        catch(IOException e){
+            System.err.println("Fail to read the file for " + columnTitle);
         }
 
-        return ipAddrList;
+        return inputList;
+    }
+
+    /**
+     * 
+     * @param filePath
+     * @return
+     */
+    public List<String> getIpAddrList(String filePath) {
+        final int ipAddrPositionNum = 2;
+        final String columnTitle = "IpAddress";
+        return getColumnInfoList(filePath, ipAddrPositionNum, columnTitle);
     }
 
     /**
@@ -46,27 +58,8 @@ public class CsvReader {
      * @return
      */
     public List<String> getHostNameList(String filePath) {
-        final int csvColumnCount = 4;
         final int hostNamePositionNum = 1;
-        List<String> hostNameList = new ArrayList<>();
-
-        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
-            String line;
-            while((line = br.readLine()) != null){
-                String[] column = line.split(",");
-
-                if(column.length > csvColumnCount - 1 && column[1].trim().isEmpty()){
-                    System.out.println("Warning: Missing the 3rd value (Hostname)");
-                }
-                if(column.length > csvColumnCount - 1){
-                    hostNameList.add(column[hostNamePositionNum]);
-                }
-            }
-        }
-        catch(IOException e){
-            System.out.println("getHostNameList error");
-        }
-
-        return hostNameList;
+        final String columnTitle = "Hostname";
+        return getColumnInfoList(filePath, hostNamePositionNum, columnTitle);
     }
 }
