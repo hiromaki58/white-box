ffaf9abcaec53fdba7bc8a0323ccbc6c173acf55
Hiro Maki <hiromaki58@gmail.com>
2024-10-19 23:06:26 -0400

    [add]add the red hat command list

--- linux_updating/src/main/java/springframework/linuxUpdating/utils/CommandList.java
+++ linux_updating/src/main/java/springframework/linuxUpdating/utils/CommandList.java
@@ -8,10 +8,27 @@ public class CommandList {
     public static List<CommandSet> getUbuntuCommandList(){
         List<CommandSet> commandList = new ArrayList<>();
         commandList.add(new CommandSet("hostname", true, false));
+        commandList.add(new CommandSet("cat /etc/issue", true, false));
         commandList.add(new CommandSet("sudo apt update", false, false));
         commandList.add(new CommandSet("sudo apt upgrade", false, true));
         commandList.add(new CommandSet("ps aux | grep apache2 | grep -v grep", true, false));
 
         return commandList;
 	}
+
+    public static List<CommandSet> getRedHatCommandList(){
+        List<CommandSet> commandList = new ArrayList<>();
+        commandList.add(new CommandSet("hostname", true, false));
+        commandList.add(new CommandSet("cat /etc/redhat-release", true, false));
+        commandList.add(new CommandSet("ps -afw | grep apache2 | grep -v grep", false, false));
+        commandList.add(new CommandSet("netstat -apnt | grep apache2 | grep -v grep", false, false));
+        commandList.add(new CommandSet("yum check-update", false, false));
+        commandList.add(new CommandSet("yum update", false, true));
+
+        commandList.add(new CommandSet("ps -afw | grep apache2 | grep -v grep", false, false));
+        commandList.add(new CommandSet("netstat -apnt | grep apache2 | grep -v grep", false, false));
+        commandList.add(new CommandSet("yum check-update", false, false));
+
+        return commandList;
+    }
 }
