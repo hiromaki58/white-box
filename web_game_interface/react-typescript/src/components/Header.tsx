d9e1ebe14753503a42792569c27a38362bc17723
Hiro Maki <hiromaki58@gmail.com>
2024-12-02 23:31:29 -0500

    [add]react header

--- web_game_interface/react-typescript/src/components/Header.tsx
+++ web_game_interface/react-typescript/src/components/Header.tsx
@@ -9,20 +9,50 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ isLoggedIn, onLoginClick }) => {
   return (
     <header className="header">
-      <div className="header-content">
-        <img src="/images/logo.jpg" alt="Logo" className="logo" />
-        <nav>
+      <div className="header-in only-sp">
+        <section>
+          <h1 className="header-logo logo-company">
+            <a href="https://"><img src="/img/logo_institution_01.jpg" alt="https://"/></a>
+          </h1>
+          <nav className="nav-global">
           <ul>
-            {isLoggedIn ? (
-              <>
-                <li><a href="/profile">Profile</a></li>
-                <li><a href="/logout">Logout</a></li>
-              </>
-            ) : (
-              <li><button onClick={onLoginClick}>Login</button></li>
-            )}
-          </ul>
-        </nav>
+              {isLoggedIn ? (
+                <>
+                  <li><a href="#"><span className="nav-global-in-first">logout</span></a></li>
+                  <li><a href="#"><span className="nav-global-in">profile</span></a></li>
+                </>
+              ) : (
+                <>
+                  <li><a href="#"><span className="nav-global-in-first">login</span></a></li>
+                  <li><a href="#"><span className="nav-global-in">registration</span></a></li>
+                </>
+              )}
+            </ul>
+          </nav>
+        </section>
+      </div>
+
+      <div className="header-in only-pc clearfix">
+        <section>
+          <h1 className="header-logo logo-company">
+            <a href="https://"><img src="/img/logo_institution_01.jpg" alt="https:///" /></a>
+          </h1>
+          <nav className="nav-global">
+            <ul>
+              {isLoggedIn ? (
+                <>
+                  <li><a href="#"><span className="nav-global-in-first">logout</span></a></li>
+                  <li><a href="#"><span className="nav-global-in">profile</span></a></li>
+                </>
+              ) : (
+                <>
+                  <li><a href="#"><span className="nav-global-in-first">login</span></a></li>
+                  <li><a href="#"><span className="nav-global-in">registration</span></a></li>
+                </>
+              )}
+            </ul>
+          </nav>
+        </section>
       </div>
     </header>
   );
