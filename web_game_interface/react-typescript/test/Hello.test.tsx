54e31b09ad993f88191769f397fcc1556110a466
Hiro Maki <hiromaki58@gmail.com>
2024-12-15 19:02:56 -0500

    [change]testing vitest is fine

--- web_game_interface/react-typescript/test/Hello.test.tsx
+++ web_game_interface/react-typescript/test/Hello.test.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { describe, test, expect } from "vitest"; // Vitestの関数をインポート
 import Hello from "./Hello";
 
-test("renders Hello, World!", () => {
-  render(<Hello />);
-  const headingElement = screen.getByText("Hello, World!");
-  expect(headingElement).toBeInTheDocument();
+describe("Hello Component", () => {
+  test("renders Hello, World!", () => {
+    render(<Hello />);
+    const headingElement = screen.getByText("Hello, World!");
+    expect(headingElement).toBeInTheDocument();
+  });
 });
--- web_game_interface/react-typescript/vitest.config.ts
+++ web_game_interface/react-typescript/vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globals: true, // expectやtestをグローバルに使用可能にする
+    environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
   },
 });
