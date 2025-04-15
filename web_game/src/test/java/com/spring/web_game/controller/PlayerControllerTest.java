35e6be3ca739a6679e16a44faa3436a0517e33d6
Hiro Maki <hiromaki58@gmail.com>
2025-04-14 21:18:24 -0400

    [add] test properties file for testing

--- web_game/src/test/java/com/spring/web_game/controller/PlayerControllerTest.java
+++ web_game/src/test/java/com/spring/web_game/controller/PlayerControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.spring.web_game.model.PlayerModel;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PlayerControllerTest {
