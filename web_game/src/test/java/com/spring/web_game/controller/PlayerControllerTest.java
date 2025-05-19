048e343208200ddc0e9404bf01720ea078009495
Hiro Maki <hiromaki58@gmail.com>
2025-05-19 14:58:53 -0400

    [change] add java mail sender to the player controller test

--- web_game/src/test/java/com/spring/web_game/controller/PlayerControllerTest.java
+++ web_game/src/test/java/com/spring/web_game/controller/PlayerControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
@@ -27,6 +26,8 @@ public class PlayerControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private PlayerRepository playerRepository;
+    @MockitoBean
+    private JavaMailSender javaMailSender;
 
     @BeforeEach
     public void setUp() {
