136b60a2c52fa35bebdc36e605952465633cdd95
Hiroyuki Maki <hiromaki58@gmail.com>
2025-11-05 11:54:54 -0500

    [add]start minikube with mysql project and creat the back end boilerplate

---
+++ infra/minikube/mysql-minikube-backend/src/main/java/com/minikube/mysql_minikube/MysqlMinikubeApplication.java
@@ -0,0 +1,13 @@
+package com.minikube.mysql_minikube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlMinikubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlMinikubeApplication.class, args);
+	}
+
+}
---
+++ infra/minikube/mysql-minikube-backend/src/test/java/com/minikube/mysql_minikube/MysqlMinikubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minikube.mysql_minikube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlMinikubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
