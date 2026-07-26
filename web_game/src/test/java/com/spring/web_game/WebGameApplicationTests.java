06e7541b7b5aeaf7b37a7ffe76fbf8714d27e672
Hiroyuki Maki <hiromaki58@gmail.com>
2026-07-26 19:56:46 -0400

    [change] Succeed to pass the login/logoff test with local H2 DB.

--- web_game/src/test/java/com/spring/web_game/WebGameApplicationTests.java
+++ web_game/src/test/java/com/spring/web_game/WebGameApplicationTests.java
@@ -3,9 +3,11 @@ package com.spring.web_game;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebGameApplicationTests {
 	@MockBean
     private JavaMailSender javaMailSender;
